d013432c091083c382a8d91020b1d06b42724cd2
Tadeu17 <17.tadeu.1989@gmail.com>
2025-03-30 21:09:30 +0100

    chore: removed unnecessary code from location

--- src/domain/entities/Location.ts
+++ src/domain/entities/Location.ts
@@ -5,9 +5,3 @@ export type Location = {
   latitude: number;
   longitude: number;
 };
-
-export const HARDCODED_LOCATIONS: Location[] = [
-  { id: "lisbon", name: "Lisbon", country: "Portugal", latitude: 38.7169, longitude: -9.1399 },
-  { id: "funchal", name: "Funchal", country: "Portugal", latitude: 32.6496, longitude: -16.9086 },
-  { id: "paris", name: "Paris", country: "France", latitude: 48.8566, longitude: 2.3522 },
-] as const;
