f110ad222f5b3d65d13fcf3a6c32b7df6b6db7cd
Tadeu17 <17.tadeu.1989@gmail.com>
2025-03-30 22:37:22 +0100

    feat: weather service requesting from repository

--- src/application/services/WeatherService.ts
+++ src/application/services/WeatherService.ts
@@ -4,7 +4,13 @@ import { WeatherRepository } from "@/infrastructure/repositories/WeatherReposito
 
 export const getCurrentWeather = async (location: Location): Promise<Weather | null> => {
   try {
-    return await WeatherRepository.fetchCurrentWeather(location);
+    const result = await WeatherRepository.fetchCurrentWeather(location);
+
+    if (!result) {
+      throw new Error('Weather data not available');
+    }
+
+    return result;
   } catch (error) {
     console.error("Error fetching current weather:", error);
     return null;
@@ -12,9 +18,23 @@ export const getCurrentWeather = async (location: Location): Promise<Weather | n
 };
 
 export const getHistoricalWeather = async (location: Location, dateEnd: Date): Promise<Weather | null> => {
+  const dateStart = new Date(dateEnd);
+  dateStart.setDate(dateEnd.getDate() - 7);
+
   try {
-    const dateStart = new Date(dateEnd);
-    dateStart.setDate(dateEnd.getDate() - 7);
+
+    return await WeatherRepository.fetchHistoricalWeather(location, dateStart, dateEnd);
+  } catch (error) {
+    console.error("Error fetching historical weather:", error);
+    return null;
+  }
+};
+
+export const getForecastedWeather = async (location: Location, dateStart: Date): Promise<Weather | null> => {
+  const dateEnd = new Date(dateStart);
+  dateEnd.setDate(dateEnd.getDate() + 7);
+
+  try {
 
     return await WeatherRepository.fetchHistoricalWeather(location, dateStart, dateEnd);
   } catch (error) {
